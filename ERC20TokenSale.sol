8824fced20f2527aaa0b0d1f41ffa300e0f70042
Suthan Somadeva <106453938+PeakyCryptos@users.noreply.github.com>
2022-08-09 22:24:03 -0400

    Update ERC20TokenSale.sol

--- ERC20TokenSale.sol
+++ ERC20TokenSale.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract TokenSale is ERC20Capped, Ownable {
     constructor(uint256 initialSupply) 
     ERC20("TokenSale", "TS") 
-    ERC20Capped(1000000*10**18) // All mints capped to 1 mil tokens
+    ERC20Capped(1_000_000*10**18) // All mints capped to 1 mil tokens
     {
         _mint(msg.sender, initialSupply);
     }
@@ -16,11 +16,11 @@ contract TokenSale is ERC20Capped, Ownable {
     function mintTokens() external payable {
         // Check if total supply already reached 
         uint256 totalSupply = totalSupply();
-        require(totalSupply < 1000000 * 10**18);
+        require(totalSupply < 1_000_000 * 10**18);
 
         // 1000 tokens per 1 eth, refund excess to user
         uint256 excess = msg.value % 10**18; // fractional porition
-        uint256 amount = (msg.value / 10**18) * 10**18; // get integer value
+        uint256 amount = (msg.value / 10**18) * 10**18; // get integer value (Intentionally discarding numbers after decimal point)
         _mint(msg.sender, amount * 1000);
 
         if (excess != 0) {
